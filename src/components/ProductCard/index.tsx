a22f64351cb3940a6152fa27b758f285401621d7
arlindrexhepi <axr3649@g.rit.edu>
2022-04-13 23:46:42 +0200

    Product-card and product-card-skeleton animation

---
+++ src/components/ProductCard/index.tsx
@@ -0,0 +1,64 @@
+import { Link } from "react-router-dom";
+import { FaShoppingCart, FaHeart } from "react-icons/fa";
+
+import RatingStars from "./RatingStars";
+import Button from "../ui/Button";
+import Card from "../ui/Card";
+
+const ProductCard: React.FC = () => {
+  const link =
+    "https://demo2wpopal.b-cdn.net/technocy/wp-content/uploads/2021/07/11-460x460.jpg";
+  return (
+    <Card
+      className="product-card-container w-[320px] hover:shadow-xl relative 
+      flex flex-col
+      justify-center my-4"
+    >
+      <div
+        className="absolute w-full px-4 py-2 left-0 top-0 flex items-center
+        justify-between rounded-tl rounded-tr"
+      >
+        <p>Computers &amp; Laptops</p>
+      </div>
+      <Link to="/">
+        <img className="object-contain p-2" src={link} alt="laptop" />
+      </Link>
+      <div className="px-4">
+        <RatingStars rating={5} />
+      </div>
+      <div className="product-card-title px-4">
+        <p>
+          Touchscreen, AMD Ryzen 5 5500U Processor 17.3″ HD+ Touchscreen, AMD
+          Ryzen 5 5500U Processor, 17.3″ HD+
+        </p>
+      </div>
+      <div className="flex items-baseline px-4 pt-2 pb-4">
+        <span className="line-through mr-2 font-semibold text-muted text-lg">
+          1500.00 &euro;
+        </span>
+        <span className="font-semibold text-secondary text-2xl">
+          1400.00 &euro;
+        </span>
+      </div>
+      <div
+        className="product-card-button shadow-xl rounded-bl rounded-br bg-white 
+        absolute top-[100%] px-4 flex items-center py-4 justify-center w-full"
+      >
+        <Button
+          className="flex items-center justify-center bg-secondary rounded
+          hover:bg-darkSecondary font-semibold py-2 px-4 text-white w-[80%]"
+        >
+          ADD TO CART
+          <FaShoppingCart className="ml-4" />
+        </Button>
+        <Button
+          className="flex items-center ml-1 justify-center bg-secondary rounded
+          hover:bg-darkSecondary font-semibold py-3 px-4 text-white w-[20%]"
+        >
+          <FaHeart />
+        </Button>
+      </div>
+    </Card>
+  );
+};
+export default ProductCard;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,27 @@
+import { useState, useEffect, useMemo } from "react";
 import useWindowResize from "../hooks/useWindowResize";
 import useBannerService from "../hooks/useBannerService";
+import randomArray from "../utils/randomArray";
 
 import Carousel from "../components/Carousel";
+import ProductCard from "../components/ProductCard";
+import ProductCardSkeleton from "../components/ui/ProductCardSkeleton";
 
 const Home: React.FC = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const { windowInnerWidth } = useWindowResize();
   const { data } = useBannerService({
     url: "/data/banners.json"
   });
+  const productsNumber = 22;
+  const newArr = useMemo(() => randomArray(productsNumber), [productsNumber]);
+  useEffect(() => {
+    const loaderTimeout = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(loaderTimeout);
+  }, [data]);
   const showCarosel = (): JSX.Element | null => {
     if (data && windowInnerWidth <= 768) {
       return <Carousel imgArr={data.mobile} />;
@@ -18,7 +32,24 @@ const Home: React.FC = () => {
     return null;
   };
 
-  return <div className="main-container bg-white">{showCarosel()}</div>;
+  return (
+    <div className="main-container bg-white">
+      {showCarosel()}
+      <div
+        className="flex items-center flex-wrap gap-y-[100px] place-content-center
+       md:place-content-start side-space my-5 pt-5 pb-12"
+      >
+        {isLoading &&
+          newArr.map((el: number) => {
+            return <ProductCardSkeleton key={el} />;
+          })}
+        {!isLoading &&
+          newArr.map((el: number) => {
+            return <ProductCard key={el} />;
+          })}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
