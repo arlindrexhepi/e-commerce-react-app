778b2f182f111cc57875018e3a4699c6932eee57
arlindrexhepi <axr3649@g.rit.edu>
2022-04-22 00:33:34 +0200

    created product models and connected to cloudDB

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,10 +1,13 @@
 const asyncHandler = require("express-async-handler");
 
+const Product = require("../models/productModel");
+
 // Get All Products
 // @route GET /api/products
 // @access Private
 const getProducts = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Get All Products" });
+  const products = await Product.find();
+  res.status(200).json(products);
 });
 
 // Get All Products
@@ -15,21 +18,49 @@ const setProduct = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Please add a text field");
   }
-  res.status(200).json(req.body.text);
+  const newProduct = await Product.create({
+    text: req.body.text
+  });
+
+  res.status(200).json(newProduct);
 });
 
 // Get All Products
 // @route PUT /api/products/:id
 // @access Private
 const updateProduct = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Update Single Product" });
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    res.status(400);
+    throw new Error("Product does not exist!");
+  }
+
+  const updatedProduct = await Product.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+
+  res.status(200).json(updatedProduct);
 });
 
 // Get All Products
 // @route DELETE /api/products/:id
 // @access Private
 const deleteProduct = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Delete Single Product" });
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    res.status(400);
+    throw new Error(`Product doesn't exist!`);
+  }
+
+  await Product.findByIdAndRemove(req.params.id);
+
+  res
+    .status(200)
+    .json({ id: req.params.id, message: "Product deleted successfully!" });
 });
 
 module.exports = {
